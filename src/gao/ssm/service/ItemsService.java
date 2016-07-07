396ab37f1c8dcb16923e95eb3d022b8323859dde
gaopeng527 <1259728004@qq.com>
2016-07-07 23:03:33 +0800

    添加实现修改商品功能的业务层代码

--- src/gao/ssm/service/ItemsService.java
+++ src/gao/ssm/service/ItemsService.java
@@ -13,4 +13,10 @@ import gao.ssm.po.ItemsQueryVo;
 public interface ItemsService {
 	// 商品查询列表
 	public List<ItemsCustom> findItemsList(ItemsQueryVo itemsQueryVo) throws Exception;
+	
+	// 根据id查询商品信息
+	public ItemsCustom findItemsById(Integer id) throws Exception;
+	
+	// 更新用户信息
+	public void updateItems(Integer id, ItemsCustom itemsCustom) throws Exception;
 }
--- src/gao/ssm/service/impl/ItemsServiceImpl.java
+++ src/gao/ssm/service/impl/ItemsServiceImpl.java
@@ -2,12 +2,14 @@ package gao.ssm.service.impl;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-
 import gao.ssm.mapper.ItemsMapperCustom;
+import gao.ssm.po.Items;
 import gao.ssm.po.ItemsCustom;
 import gao.ssm.po.ItemsQueryVo;
 import gao.ssm.service.ItemsService;
+import gao.ssm.mapper.ItemsMapper;
 /**
  * 管理商品实现
  * @author DELL
@@ -17,11 +19,35 @@ public class ItemsServiceImpl implements ItemsService {
 	// 自动注入查询商品信息地mapper接口
 	@Autowired
 	private ItemsMapperCustom itemsMapperCustom;
+	@Autowired
+	private ItemsMapper itemsMapper;
 	
 	@Override
 	public List<ItemsCustom> findItemsList(ItemsQueryVo itemsQueryVo) throws Exception {
 		// 通过itemsMapperCustom查询数据库
 		return itemsMapperCustom.findItemsList(itemsQueryVo);
+	}
+
+	@Override
+	public ItemsCustom findItemsById(Integer id) throws Exception {
+		Items items = itemsMapper.selectByPrimaryKey(id);
+		// 中间对商品做些业务处理
+		// ...
+		// 返回处理后的ItemsCustom信息
+		ItemsCustom itemsCustom = new ItemsCustom();
+		BeanUtils.copyProperties(items, itemsCustom);
+		return itemsCustom;
+	}
+
+	@Override
+	public void updateItems(Integer id, ItemsCustom itemsCustom) throws Exception {
+		// 添加业务校验，通常在service接口对关键参数进行校验
+		// 校验id是否为空，如果为空抛出异常
+		
+		// updateByPrimaryKeyWithBLOBs()可以根据id更新items中的所有数据，包括大文本数据
+		// 要求必须传入id信息
+		itemsCustom.setId(id);
+		itemsMapper.updateByPrimaryKeyWithBLOBs(itemsCustom);
 	}
 
 }

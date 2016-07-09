5eb1bcdbd8410aec10e71a6bb378eb2e47427663
gaopeng527 <1259728004@qq.com>
2016-07-09 10:29:24 +0800

    自定义参数绑定，类型转换器

---
+++ src/gao/ssm/controller/converter/CustomDateConverter.java
@@ -0,0 +1,31 @@
+package gao.ssm.controller.converter;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.springframework.core.convert.converter.Converter;
+
+/**
+ * 日期类型转换器
+ * @author DELL
+ *
+ */
+public class CustomDateConverter implements Converter<String, Date>{
+
+	@Override
+	public Date convert(String source) {
+		// 实现将日期串转换为日期类型格式为yyyy-MM-dd HH:mm:ss
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		try {
+			// 转换成功直接返回
+			return simpleDateFormat.parse(source);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		// 如果参数绑定失败返回null
+		return null;
+	}
+
+}
